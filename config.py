f639e4280ebb7040875b305ed581eecc6b98bfd3
btbbq <145179751+btbbq@users.noreply.github.com>
2025-06-29 03:34:10 +0400

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg")
+PING_IMG = getenv("PING_IMG", "https://imgur.com/a/fWp2OAd")
+START_IMG = getenv("START_IMG", "https://imgur.com/a/fWp2OAd")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/T_Y_E_X")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/T_Y_E_X")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+fD9UDpzeWt9lMjU6")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+fD9UDpzeWt9lMjU6")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5904216848").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1548566939").split()))
 
 
-FAILED = "https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg"
+FAILED = "https://imgur.com/a/fWp2OAd"
